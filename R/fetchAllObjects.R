03bf687224ce306d1580a52a80358fd6893a151a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-02-01 11:11:12 -0800

    fetchAllObjects now ignores redirection targets by default.
    This simplifies the return value and avoids duplicate objects unless the
    user knows what they're doing and explicitly asks for them.

--- R/fetchAllObjects.R
+++ R/fetchAllObjects.R
@@ -1,10 +1,11 @@
 #' Fetch all objects for a project
 #'
-#' Fetch and load all objects for a project or one of its versions.
+#' Fetch and load all (non-child) objects for a project or one of its versions.
 #'
 #' @param project String containing the name of the project.
 #' @param version String containing the name of the version.
 #' @param ... Further arguments to pass to \code{\link{fetchObject}}.
+#' @param ignore.redirection.target Logical scalar indicating whether redirection targets should be ignored in the returned listing.
 #'
 #' @return If \code{version} is specified, a named list is returned containing all (non-child) R objects generated loading each resource in that version of the project.
 #' The relative path to each resource is used as the name of the corresponding list element.
@@ -15,9 +16,10 @@
 #' @details
 #' Only non-child objects are returned in each list.
 #' Children are used to assemble their parent objects and are not reported here.
-#' 
-#' For redirections, a copy of the destination object is reported under the redirection's path.
-#' This means that some resources may be reported multiple times - once for each redirection, and one for its \dQuote{true} path.
+#'
+#' By default, redirection targets are ignored when reporting objects.
+#' For example, if there is a redirection from path A to path B, the redirected path A is reported in the listing, and the redirection target B is ignored.
+#' Setting \code{ignore.redirection.target=FALSE} will cause both A and B to be reported, so the same object will be present under different paths.
 #'
 #' @author Aaron Lun
 #' @examples
@@ -29,7 +31,7 @@
 #' 
 #' @export
 #' @importFrom zircon getProjectMetadata
-fetchAllObjects <- function(project, version=NULL, ...) {
+fetchAllObjects <- function(project, version=NULL, ..., ignore.redirection.target=TRUE) {
     meta <- getProjectMetadata(project, version=version, url=restURL())
 
     collected <- list()
@@ -60,14 +62,25 @@ fetchAllObjects <- function(project, version=NULL, ...) {
         if (!(v %in% names(collected))) {
             next
         }
+        current <- collected[[v]]
 
-        current.redirects <- redirects[[v]]
-        for (n in names(current.redirects)) {
-            p <- current.redirects[[n]]
-            if (p %in% names(collected[[v]])) {
-                collected[[v]][[n]] <- collected[[v]][[p]]
-            }
+        host <- names(redirects[[v]])
+        targets <- unlist(redirects[[v]])
+
+        m <- match(targets, names(current))
+        keep <- !is.na(m)
+        m <- m[keep]
+        host <- host[keep]
+
+        if (ignore.redirection.target) {
+            names(current)[m] <- host
+        } else {
+            copies <- current[m]
+            names(copies) <- host
+            current <- c(current, copies)
         }
+
+        collected[[v]] <- current
     }
 
     if (!is.null(version)) {
--- tests/testthat/test-fetchAllObjects.R
+++ tests/testthat/test-fetchAllObjects.R
@@ -4,10 +4,18 @@
 test_that("fetchAllObjects works as expected", {
     objects <- fetchAllObjects("dssc-test_basic-2023", version="2023-01-19")
     expect_s4_class(objects$my_first_sce, "SingleCellExperiment")
-    expect_identical(objects$my_first_sce, objects[["my_first_sce/experiment.json"]])
     expect_s4_class(objects$my_first_df, "DataFrame")
-    expect_identical(objects$my_first_df, objects[["my_first_df/simple.csv.gz"]])
 
     more.objects <- fetchAllObjects("dssc-test_basic-2023")
+    expect_identical(names(more.objects), "2023-01-19")
     expect_identical(objects, more.objects[[1]])
+})
+
+test_that("fetchAllObjects includes redirection targets if requested", {
+    objects <- fetchAllObjects("dssc-test_basic-2023", version="2023-01-19", ignore.redirection.target=FALSE)
+    expect_s4_class(objects$my_first_sce, "SingleCellExperiment")
+    expect_s4_class(objects$my_first_df, "DataFrame")
+
+    expect_identical(objects$my_first_sce, objects[["my_first_sce/experiment.json"]])
+    expect_identical(objects$my_first_df, objects[["my_first_df/simple.csv.gz"]])
 })
